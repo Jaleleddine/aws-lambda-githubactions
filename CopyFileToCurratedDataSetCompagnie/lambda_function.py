aa1c6ee98326a58fc7cf4e80ddc173bdb1489015
Jaleleddine <38255287+Jaleleddine@users.noreply.github.com>
2023-04-15 18:48:24 +0200

    Update lambda_function.py

--- CopyFileToCurratedDataSetCompagnie/lambda_function.py
+++ CopyFileToCurratedDataSetCompagnie/lambda_function.py
@@ -4,12 +4,11 @@ Decription :
 This Lambda function performs the following actions:
     * If the file is valid, meaning that it is in CSV format, non-empty, and has a line separator 
     of (";" or "\t") the file is transferred to the S3 bucket after converting it to UTF-8
-    "eu-totalenergies-datalake-9443-0190-4016-curated" with the following folder 
+    "xxx-datalake-9443-0190-4016-curated" with the following folder 
     structure: "eu-totalenergies-datalake-9443-0190-4016-curated/application/YYYY/MM/DD/file.csv"
       
     * Otherwise, the file is transferred to the "eu-totalenergies-datalake-9443-0190-4016-error" 
     bucket with the following folder structure: 
-      eu-totalenergies-datalake-9443-0190-4016-error/application/YYYY/MM/DD/file.csv
       
 
 
